045a363c4d22d81237836d0c4c77ac91ad96eb57
mrigakshi-chib <mrigakshichib@gmail.com>
2026-06-23 17:19:31 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ''' brute force
+        if sorted(s) == sorted(t):
+            return True
+        return False '''
+
+        '''optimised'''
+        if len(s) != len(t):
+            return False
+        count = {}
+
+        for ch in s:
+            count[ch] = count.get(ch, 0) + 1
+
+        for ch in t:
+            if ch not in count:
+                return False
+            count[ch] -=1
+
+            if count[ch] < 0:
+                return False
+        return True
+
+
+        
+
+
